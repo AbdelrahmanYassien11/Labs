eb6f72c186df997f4fbdcdb4b28c78fc97bff349
AbdelrahmanYassien11 <Abdelrahman.Yassien11@gmail.com>
2025-06-21 23:55:57 +0300

    ...Final

--- alu_uvm_project/dv/env/env_components/v_sequencer.sv
+++
@@ -1,9 +0,0 @@
-class virtual_sequencer extends uvm_sequencer;
-  `uvm_component_utils(virtual_sequencer)
-  alu_sequencer		  seqr_ALU;
-  rst_sequencer 	  seqr_RST;
-
-  function new(string name = "virtual_sequencer", uvm_component parent = null);
-    super.new(name, parent);
-  endfunction
-endclass 
